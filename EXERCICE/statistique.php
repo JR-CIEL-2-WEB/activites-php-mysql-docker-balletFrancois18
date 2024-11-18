c90ab0d0d586c4dc2f7943d5db938de7ade8fda8
balletFrancois18 <144659597+balletFrancois18@users.noreply.github.com>
2024-11-18 17:01:03 +0000

    eh

--- EXERCICE/statistique.php
+++ EXERCICE/statistique.php
@@ -1,7 +1,16 @@
 <?php
 // Fonction pour calculer la médiane d'un tableau de nombres
 function mediane($tableau) {
-   
 
+    if ($n % 2 == 1) {
+        // Si impaire, on retourne l'élément du milieu
+        return $tableau[floor($n / 2)];
+    } else {
+        // Si paire, on retourne la moyenne des deux éléments du milieu
+        $milieu1 = $tableau[$n / 2 - 1];
+        $milieu2 = $tableau[$n / 2];
+        return ($milieu1 + $milieu2) / 2;
+    }
+   
 }
 ?>
